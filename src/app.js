8b891500cca70f0e2585aa11305398a9262f8623
m t <Shawn.Dougherty.NJ@Gmail.com>
2018-11-08 19:15:36 -0500

    Added Controller Logic [Get & GetAll]

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('../config/config');
 const models = require('../models');
+const sessions = require('../controllers/SessionsController');
 
 const app = express();
 
@@ -19,4 +20,22 @@ if (CONFIG.app == 'dev' ){
     models.sequelize.sync();
 }
 
+app.get('/sessions', sessions.getAll);
+app.get('/sessions/:sessionId', sessions.get);
+
 module.exports = app;
+
+const get = (req, res) => {
+    let sessionId = parseInt(req.params.sessionId);
+    res.setHeader('Content-Type', 'application/json');
+
+    let sessions = [{ Id: 1, Name: 'John Teaches Angular', Location: 'Miles-U 1' },
+    { Id: 2,Name: 'Scott Teaches AWS', Location: 'Miles-U 2' },
+    { Id: 3,Name: 'Jack Teaches PODIS', Location: 'Jacks Desk' },
+    ];
+
+    let session = sessions.find(obj => obj.Id === sessionId);
+    console.log(session);
+    return res.json(session);
+};
+module.exports.get = get;
