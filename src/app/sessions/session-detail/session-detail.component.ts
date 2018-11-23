d92612137d94b2bee7676120ef5ae6ea21fab088
m t <Shawn.Dougherty.NJ@Gmail.com>
2018-11-23 17:58:49 -0500

    Updated Session Detail Component

--- src/app/sessions/session-detail/session-detail.component.ts
+++ src/app/sessions/session-detail/session-detail.component.ts
@@ -1,38 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { SessionsService, ISession } from '../sessions.service';
-
-const defaultSession:  ISession = {
-  id: 0,
-  name: '',
-  location: '',
-  startTime: new Date(),
-  createdAt: null,
-  updatedAt: null,
-};
-
-@Component({
-  templateUrl: './session-detail.component.html',
+import { ActivatedRoute, Router } from '@angular/router';
+import { ISession, SessionsService } from '../sessions.service';
+ @Component({
+    templateUrl: './session-detail.component.html',
 })
-export class SessionDetailComponent implements OnInit {
-
-session: ISession = {...defaultSession};
-// TODO CCC: this is hard coded and should be coming from
-// the new Date(), butISO String not working right
-// fix later
-startTimeAsString = '2018-11-15T23:34';
-
-  constructor(
-    private sessionsService: SessionsService,
+export class SessionsDetailComponent implements OnInit {
+     session: ISession;
+     constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private sessionsService: SessionsService,
     ) { }
-
-  ngOnInit() {
-  }
-
-
-  submit(): void {
-    console.log(this.session);
-    console.log(this.sessionsService);
-    this.sessionsService.createSessions(this.session)
-      .subscribe();
-  }
-}
+     ngOnInit() {
+        let id: string | number = this.route.snapshot.paramMap.get('sessionId');
+        // tslint:disable-next-line:radix
+        id = isNaN(parseInt(id)) ? 0 : parseInt(id);
+        if (id > 0) {
+            // get from db
+            this.sessionsService.getSessionById(id)
+                .subscribe((session) => {
+                    const startTime = new Date(session.startTime);
+                    startTime.setHours(startTime.getHours() - (startTime.getTimezoneOffset() / 60));
+                    session.startTime = startTime.toISOString().slice(0, 16);
+                    this.session = session;
+                });
+        } else {
+            // new
+            this.session = {
+                id: 0,
+                name: '',
+                location: '',
+                startTime: this.getLocalDateTime(),
+                createdAt: '',
+                updatedAt: '',
+            };
+        }
+    }
+     getLocalDateTime(): string {
+        const startTime = new Date();
+        startTime.setHours(startTime.getHours() - (startTime.getTimezoneOffset() / 60));
+        return startTime.toISOString().slice(0, 16);
+    }
+     save(): void {
+        if (!this.formValid()) {
+            // TODO CCC: pop message about not valid
+            console.log('form invalid');
+            return;
+        }
+        this.sessionsService.save(this.session)
+            .subscribe((session) => {
+                // TODO CCC: add a success message
+                this.router.navigate(['sessions']);
+            });
+    }
+     private formValid(): boolean {
+        return this.session.name && this.session.location ? true : false;
+    }
+     cancel(): void {
+        this.router.navigate(['sessions']);
+    }
+ }
