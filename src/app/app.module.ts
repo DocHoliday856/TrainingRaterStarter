6aa5b7432c2d99f6d71441bf02de37308293b26d
m t <Shawn.Dougherty.NJ@Gmail.com>
2018-12-05 17:30:30 -0500

    Lessons in Anatomy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// stuff from 3rd party
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastModule } from 'ng2-toastr/ng2-toastr';
 
+// stuff we created
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { SessionsModule } from './sessions/sessions.module';
 import { UsersModule } from './users/users.module';
 
--- src/app/sessions/sessions.module.ts
+++ src/app/sessions/sessions.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { SessionsListComponent } from './sessions-list/sessions-list.component';
-import { SessionsService } from './sessions.service';
-import { CommonModule } from '@angular/common';
 import { SessionsDetailComponent } from './session-detail/session-detail.component';
-import { FormsModule } from '@angular/forms';
 import { SessionRatingComponent } from './session-rating/session-rating.component';
+
 import { SessionRatingsService } from './session-rating/session-ratings.service';
+import { SessionsService } from './sessions.service';
 
 @NgModule({
     declarations: [
